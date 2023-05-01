3b9fb723b877d58d1dd3266bd711f2590615f177
GE-Developer <on-line-misha@mail.ru>
2023-05-01 16:35:12 +0400

    Initial Commit

---
+++ Scrambler/ScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScramblerApp.swift
+//  Scrambler
+//
+//  Created by M I C H A E L on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
